1ee8c1631cd7c9b9f937eee36abd24ba6080f40e
emildastik-cyber <emildastik@gmail.com>
2025-10-09 17:13:27 +0600

    Add files via upload

---
+++ Task4.java
@@ -0,0 +1,57 @@
+// import java.util.Scanner;
+// public class Task4 {
+//   public static void main(String[] args) {
+//     Scanner scanner = new Scanner(System.in);
+    
+//     double a = scanner.nextDouble();
+//     double b = scanner.nextDouble(); 
+    
+//     int a1 = (int)(a*1000);
+//     int b1 = (int)(b*1000);
+    
+//     if (a1 == b1) {
+//         System.out.print("Same");
+//     } else {
+//         System.out.print("Not Same");
+//     }
+//   }
+//  static void exercise3(Scanner scanner){
+    //  int a = scanner.nextInt();
+    // if (a % 400 == 0 || a % 4 == 0 && a % 100 != 0) {
+    //     System.out.print("true");
+    // } else {
+    //     System.out.print("false");
+    // }
+//    }
+    
+    
+//  static void exercise2(Scanner scanner){
+    // int a = scanner.nextInt();
+    // int b = scanner.nextInt();
+    // int c = scanner.nextInt();
+    
+    
+    // if (a % 2 == 0 && b % 2 == 0) {
+    //     System.out.print("true");
+    // } else if (a % 2 == 0 && c % 2 == 0) {
+    //     System.out.print("true");
+    // } else if (b % 2 == 0 && c % 2 == 0) {
+    //     System.out.println("true");
+    // } else {
+    //     System.out.println("false");
+    // }
+  //}
+    
+    
+//   static void exercise1(Scanner scanner){
+    //  int a = scanner.nextInt();
+    // int b = scanner.nextInt();
+    
+    
+    // if ((a+b) % 2 == 0 && (a*b)%2!=0) {
+    //     System.out.print("true");
+    // } else {
+    //     System.out.print("false");
+    // }
+//    }
+//}
